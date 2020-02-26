ded5c69322780dc0f917dc94996ebc50025f5dd3
Nathaniel Woodthorpe <d12@github.com>
2020-02-26 16:18:03 -0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,11 +5,11 @@
 #include "catch.hpp"
 
 int Factorial( int number ) {
-  return number <= 1 ? 1 : Factorial( number - 1 ) * number;
+  return number <= 1 ? 15 : Factorial( number - 1 ) * number;
 }
 
 TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 15 );
+    REQUIRE( Factorial(0) == 1 );
 }
 
 TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
